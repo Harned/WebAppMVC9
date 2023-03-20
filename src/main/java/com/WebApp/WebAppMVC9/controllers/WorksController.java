1351e029a86059d2b81b31f57c81e84d79ebd0b7
Dmitry <aleshkindima25@gmail.com>
2023-03-21 01:24:18 +0600

    Попытка от 20.03
    - Создана вторая сущность Job
    - Поключена к базе данных, реализованы CRUD команды

--- src/main/java/com/WebApp/WebAppMVC9/controllers/WorksController.java
+++ src/main/java/com/WebApp/WebAppMVC9/controllers/WorksController.java
@@ -10,61 +10,61 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/work")
-public class WorkController {
+@RequestMapping("/works")
+public class WorksController {
 
     private final JobDAO jobDAO;
     @Autowired
-    public WorkController(JobDAO jobDAO) {
+    public WorksController(JobDAO jobDAO) {
         this.jobDAO = jobDAO;
     }
 
     @GetMapping()
     public String index(Model model) {
-        model.addAttribute("work", jobDAO.index());
-        return "work/index";
+        model.addAttribute("works", jobDAO.index());
+        return "works/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("job", jobDAO.show(id));
-        return "work/show";
+        return "works/show";
     }
 
     @GetMapping("/new")
     public String newJob(@ModelAttribute("job")  Job job) {
-        return "work/new";
+        return "works/new";
     }
 
     @PostMapping()
     public String create(@ModelAttribute("job") @Valid Job job,
                          BindingResult bindingResult) {
         if (bindingResult.hasErrors())
-            return "work/new";
+            return "works/new";
 
         jobDAO.save(job);
-        return "redirect:/work";
+        return "redirect:/works";
     }
 
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("job", jobDAO.show(id));
-        return "work/edit";
+        return "works/edit";
     }
 
     @PatchMapping("/{id}")
     public String update(@ModelAttribute("job") @Valid Job job,
                          BindingResult bindingResult, @PathVariable("id") int id) {
         if (bindingResult.hasErrors())
-            return "work/edit";
+            return "works/edit";
 
         jobDAO.update(id, job);
-        return "redirect:/work";
+        return "redirect:/works";
     }
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         jobDAO.delete(id);
-        return "redirect:/work";
+        return "redirect:/works";
     }
 }
